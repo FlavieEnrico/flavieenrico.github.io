ab0cb7e6c2ca0787b318decc859acefd90ef8d8a
FlavieEnrico <flavie.enrico.74@gmail.com>
2023-04-30 16:33:05 +0200

    Test parsing

---
+++ script/haikus.js
@@ -0,0 +1,21 @@
+
+
+export default function haikusLoad(infos){
+  console.log(infos["haikus"])
+
+  var codeBlock = '<h2>Haikus</h2>';
+
+  var section = document.getElementById('haikus_js');
+  for (let index = 0; index < infos["haikus"].length; index++) {
+      const haiku = infos["haikus"][index];
+      var newHaiku='<!-- Notice: each .row is an haiku -->'+
+            '<div class="row">'+
+            haiku.haiku +
+            '</div>'+
+            '<!-- /END haiku -->'
+
+          codeBlock += newHaiku
+  }
+
+  document.getElementById('haikus_js').innerHTML = codeBlock
+}
