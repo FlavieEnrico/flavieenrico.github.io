2f8698ae9ca425b7526a2365b08bd3e295c76fd3
FlavieEnrico <flavie.enrico.74@gmail.com>
2023-05-01 17:43:57 +0200

    slay css

--- script/haikus.js
+++ script/haikus.js
@@ -7,9 +7,9 @@ function haikusLoad(infos){
   for (let index = 0; index < infos.length; index++) {
       const haiku = infos[index];
       var newHaiku='<!-- Notice: each .row is an haiku -->'+
-            '<div class="row"><p>'+
+            '<div class="row"><pre>'+
             haiku.haiku +
-            '</p></div>'+
+            '</pre></div>'+
             '<!-- /END haiku -->'
 
           codeBlock += newHaiku
