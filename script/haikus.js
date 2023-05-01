c870faa3d402162b0e5523dd5753e6f4e2bc51ab
FlavieEnrico <flavie.enrico.74@gmail.com>
2023-05-01 17:36:43 +0200

    test css

--- script/haikus.js
+++ script/haikus.js
@@ -7,9 +7,9 @@ function haikusLoad(infos){
   for (let index = 0; index < infos.length; index++) {
       const haiku = infos[index];
       var newHaiku='<!-- Notice: each .row is an haiku -->'+
-            '<div class="row"><p>'+
+            '<span class="row">'+
             haiku.haiku +
-            '</p></div>'+
+            '</span>'+
             '<!-- /END haiku -->'
 
           codeBlock += newHaiku
