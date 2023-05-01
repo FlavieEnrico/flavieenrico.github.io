add200c0be501783511d0bab2f70a0644eafdb80
FlavieEnrico <flavie.enrico.74@gmail.com>
2023-05-01 14:23:24 +0200

    AH ???

--- script/haikus.js
+++ script/haikus.js
@@ -4,8 +4,8 @@ function haikusLoad(infos){
   var codeBlock = '<h2>Haikus</h2>';
 
   var section = document.getElementById('haikus_js');
-  for (let index = 0; index < infos["haikus"].length; index++) {
-      const haiku = infos["haikus"][index];
+  for (let index = 0; index < infos.length; index++) {
+      const haiku = infos[index];
       var newHaiku='<!-- Notice: each .row is an haiku -->'+
             '<div class="row">'+
             haiku.haiku +
