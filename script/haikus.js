44cae4e6a1195c77b1e22cda0be76847e1e1314d
FlavieEnrico <flavie.enrico.74@gmail.com>
2023-05-01 14:35:57 +0200

    vlpodn^s

--- script/haikus.js
+++ script/haikus.js
@@ -1,6 +1,8 @@
 function haikusLoad(infos){
   console.log(infos)
 
+  var codeBlock = '';
+
   var section = document.getElementById('haikus_js');
   for (let index = 0; index < infos.length; index++) {
       const haiku = infos[index];
